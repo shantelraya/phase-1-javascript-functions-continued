bf2b5b550eb375283a05414d5d63de8b9a57b11a
raya <parischax@gmail.com>
2022-08-17 09:29:39 +0300

    done assignments

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(funType = "roller-skate"){
+    return `This Saturday, I want to ${funType}!`;
+}
+
+saturdayFun();
+saturdayFun("bathe my dog");
+
+
+function mondayWork(activity  = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+mondayWork();
+mondayWork("work from home");
+
+
+function wrapAdjective(wrap){
+    return function(say){
+            return `You are ${wrap}${say}${wrap}!`;
+    };
+}
+
+wrapAdjective("*")("a hard worker");
+wrapAdjective('||')('a dedicated programmer');
